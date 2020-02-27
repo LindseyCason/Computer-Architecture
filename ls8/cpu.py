1dc29be262e3debf900d0c6f33867f305866023a
Lindsey C <lindseyacason@gmail.com>
2020-02-27 17:35:34 -0600

    Day1 MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,9 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.reg = [0] * 8
+        self.pc = 0 #index or program counter
+        self.ram = [0] * 256 #Memory
 
     def load(self):
         """Load a program into memory."""
@@ -60,6 +62,28 @@ class CPU:
 
         print()
 
+    def ram_read(self, MAR):
+        return self.ram[MAR]
+
+    def ram_write(self, MDR, MAR):
+        self.ram[MAR]=MDR
+
     def run(self):
         """Run the CPU."""
-        pass
+        halted=False
+        while halted == False:
+            ir=self.ram_read(self.pc) #READ THE CURRENT PC AND STORE VALUE
+            operand_a=self.ram_read(self.pc +1) #SAVE IN CASE NEEDED
+            operand_b=self.ram_read(self.pc +2) #SAVE IN CASE NEEDED
+            if ir == 0b10000010 or ir == "LDI": #130
+                self.reg[operand_a]= operand_b
+                self.pc+=3
+            elif ir == 0b01000111 or ir == "PRN":
+                print(self.reg[operand_a])
+                print(int(f"{self.reg[operand_a]}",10))
+                self.pc +=2
+            elif ir == 0b00000001 or ir == "HLT":
+                print("Exiting")
+                halted=True
+                sys.exit(1)
+        
